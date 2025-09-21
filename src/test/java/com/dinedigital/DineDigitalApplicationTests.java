49aec0a77c5b8558eb855257dbe07e805e1d3a52
Scemworks <mohammedshadin65@gmail.com>
2025-09-21 21:53:04 +0530

    test: add initial test class for application context loading

---
+++ src/test/java/com/dinedigital/DineDigitalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dinedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
